c5768d6ee9c5efde68be08ad90ea14c8f496b45a
Ubaidnawal <ubaidnawal857@gmail.com>
2026-01-03 11:54:08 +0500

    Add files via upload

---
+++ class1.sql
@@ -0,0 +1,14 @@
+create table Movie(
+m_id int primary key auto_increment,
+name varcher(100) not null,
+duration int default 60;
+rating double default  2.0,
+createAt TIMESTAMP default CURRENT_TIMESTAMP
+);
+
+
+
+
+
+
+
