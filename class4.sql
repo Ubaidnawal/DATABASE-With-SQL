a46da2ad17e044bd2d2609d4b679c9e4555d5d7f
Ubaidnawal <ubaidnawal857@gmail.com>
2026-01-08 13:13:26 +0500

    Add files via upload

---
+++ class4.sql
@@ -0,0 +1,11 @@
+create table students (
+    students_id int,
+    name varchar(100),
+     subject varchar(100)
+     markes int );
+
+insert into students(students_id,name,subject,markes) values
+(1,"ali","computer",93),
+(2,"ahmed","maths",87),
+(3,"ubaid","science",95),
+(5,"imran","english",40)
---
+++ class5.sql
@@ -0,0 +1,21 @@
+ 
+--  step 1
+
+--  SELECT subject as sujects,
+-- round(avg(marks),1) AS Avg_Marks_Subjects
+-- From marks
+-- GROUP BY subject
+-- having avg(marks)=85
+-- order by avg(marks) desc;\
+
+-- step 2
+
+-- select subject as subjects,
+-- round(avg(marks),) as Avg_Marks_Subjects
+-- From marks
+-- where not subject = 'english'
+-- GROUP by subject 
+-- having avg(marks) >=85
+-- order by avg(marks) desc;
+
+
